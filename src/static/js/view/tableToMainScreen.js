3d34c860a9c00599789a95667b2a931be8650869
Henrique Patricio de Souza da Silva <henriquepworking@gmail.com>
2025-01-02 16:36:25 -0300

    refactor: refactory table logic

--- src/static/js/view/tableToMainScreen.js
+++ src/static/js/view/tableToMainScreen.js
@@ -1,69 +1,59 @@
-const table = document.getElementById('table_data')
-const tbody = document.getElementById('tbody')
-const tfoot = document.getElementById('tfoot')
-const chartBar = document.getElementById('graphicBar')
-const chartPie = document.getElementById('graphicPie')
-const allTableRows = document.querySelectorAll(".tr_data")
-let trFoot = null, thFoot = null
-let hasContent = false
+const table = document.getElementById('table_data');
+const tbody = document.getElementById('tbody');
+const tfoot = document.getElementById('tfoot');
+const allTableRows = document.querySelectorAll(".tr_data");
+const chartBar = document.getElementById('graphicBar');
+const chartPie = document.getElementById('graphicPie');
+let thFoot = null;
+let hasContent = false;
+
 export function createTableWithData(data, localName, periodName, periodId) {
-    if(hasContent){
-        for(const child of tbody.children){
-            for(const childOfChild of child.children){
-                if(childOfChild.tagName != "TH"){
-                    childOfChild.textContent = ""    
-                }
-            }
-        }
-        thFoot.textContent = `Fonte: IBGE - Índice Nacional de Preços ao Consumidor ${localName} - ${periodName}` 
-    }else{
-        trFoot = document.createElement('tr')
-        thFoot = document.createElement('th')
-        thFoot.colSpan = 11
-        trFoot.appendChild(thFoot)
-        tfoot.appendChild(trFoot)
-    }
+    const nameTable = document.getElementById('name_table');
+    nameTable.textContent = `${localName} - ${periodName}`;
     
-    const nameTable = document.getElementById('name_table')
-    nameTable.textContent = `${localName} - ${periodName}`
+    table.style.display = 'block';
+    chartBar.style.display = 'block';
+    chartPie.style.display = 'block';
 
-    table.style.display = 'block' 
-    chartBar.style.display = 'block'
-    chartPie.style.display = 'block'
-
-    if(!hasContent){
-        let counter = 0
-        for(let i in data){
-            for(let j in data[i].resultados){ 
-                const td = document.createElement('td')   
-                let tr = allTableRows[counter]
+    if (!hasContent) {
+        let counter = 0;
+        for (let i in data) {
+            for (let j in data[i].resultados) {
+                const tr = allTableRows[counter];
+                
                 const value = data[i].resultados[j].series[0].serie[periodId];
-                td.textContent = value
-                tr.appendChild(td)
-                tbody.appendChild(tr)
+                const td = document.createElement('td');
+                td.textContent = value;
+
+                td.setAttribute('data-row', counter);
+                td.setAttribute('data-column', j);
+                
+                tr.appendChild(td);
+                tbody.appendChild(tr);
             }
-            counter++
+            counter++;
         }
-        thFoot.textContent = `Fonte: IBGE - Índice Nacional de Preços ao Consumidor ${localName} - ${periodName}`
-        thFoot.colSpan = 11
-        hasContent = true
-    }else{
-        let counter = 0
-        let childrenCounter = 1
-        for(let i in data){
-            for(let j in data[i].resultados){    
-                let tr = allTableRows[counter]
-                const child = tr.children[childrenCounter]
-                if(child.tagName !== "TH"){
-                    const value = data[i].resultados[j].series[0].serie[periodId];
-                    child.textContent = value 
+
+        thFoot = document.createElement('th');
+        thFoot.colSpan = 11;
+        thFoot.textContent = `Fonte: IBGE - Índice Nacional de Preços ao Consumidor ${localName} - ${periodName}`;
+        const trFoot = document.createElement('tr');
+        trFoot.appendChild(thFoot);
+        tfoot.appendChild(trFoot);
+        hasContent = true;
+    } else {
+        let counter = 0;
+        for (let i in data) {
+            for (let j in data[i].resultados) {
+                const value = data[i].resultados[j].series[0].serie[periodId];
+                
+                const td = tbody.querySelector(`td[data-row="${counter}"][data-column="${j}"]`);
+                if (td) {
+                    td.textContent = value;
                 }
-                childrenCounter++
             }
-            childrenCounter = 1
-            counter++
+            counter++;
         }
     }
-    
+    thFoot.textContent = `Fonte: IBGE - Índice Nacional de Preços ao Consumidor ${localName} - ${periodName}`;
 }
-
